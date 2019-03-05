915315b0590886ac4012081a9765e312b158b3e2
Xie Yuheng <xyheme@gmail.com>
2019-03-05 13:27:17 +0800

    up

--- cicada/src/main/scala/cg.scala
+++ cicada/src/main/scala/cg.scala
@@ -3,22 +3,18 @@
 // - complete information
 // - partizan game
 
-trait game_t {
-  type position_t
-  type choice_t
-  val init_position: position_t
-  def L_choices (position: position_t): List [choice_t]
-  def R_choices (position: position_t): List [choice_t]
-  def L_move (choice: choice_t, position: position_t): position_t
-  def R_move (choice: choice_t, position: position_t): position_t
-}
+class game_t [position_t, choice_t] (
+  var L_choices: (position_t) => List [choice_t],
+  var R_choices: (position_t) => List [choice_t],
+  var L_move: (choice_t, position_t) => position_t,
+  var R_move: (choice_t, position_t) => position_t,
+)
 
-class int_game_t extends game_t {
-  type position_t = Int
-  type choice_t = Int
-  val init_position = 0
-  def L_choices (x: Int) = List (1, 2, 3)
-  def R_choices (x: Int) = List (1, 2, 3)
-  def L_move (x: Int, y: Int) = 1
-  def R_move (x: Int, y: Int) = 1
-}
+class int_game_t (
+  var k: Int
+) extends game_t [Int, Int] (
+  L_choices = (x) => List (1, 2, 3),
+  R_choices = (x) => List (1, 2, 3),
+  L_move = (x, y) => 1,
+  R_move = (x, y) => 1,
+)
