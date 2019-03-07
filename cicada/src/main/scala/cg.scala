7dcbde5b964fa35f15ae69236f48fe0895e11152
Xie Yuheng <xyheme@gmail.com>
2019-03-07 20:47:46 +0800

    up

--- cicada/src/main/scala/cg.scala
+++ cicada/src/main/scala/cg.scala
@@ -8,7 +8,13 @@ class game_t [position_t, choice_t] (
   var R_choices: (position_t) => List [choice_t],
   var L_move: (choice_t, position_t) => position_t,
   var R_move: (choice_t, position_t) => position_t,
-)
+) {
+  // def sum () = {
+  // }
+
+  // def rev () = {
+  // }
+}
 
 class int_game_t (
   var k: Int
