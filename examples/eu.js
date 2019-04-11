359975ef0cd783a95e8c1cdec1b329c414875f2f
Xie Yuheng <xyheme@gmail.com>
2019-04-11 16:59:42 +0800

    up

--- examples/eu.js
+++ examples/eu.js
@@ -1,3 +1,5 @@
+let assert = require ("assert")
+
 let eu = require ("cicada-lang/lib/euclidean-space")
 let log = require ("cicada-lang/lib/util") .log
 
@@ -7,29 +9,50 @@ let log = require ("cicada-lang/lib/util") .log
     [-1,  2],
   ])
 
+  let b = eu.vector_t.from_array ([
+    0,
+    3,
+  ])
+
   let v = eu.vector_t.from_array ([
     1,
     2,
   ])
 
+
   m.table ()
   v.table ()
   m.act (v) .table ()
+
+  assert (
+    m.act (v) .eq (b)
+  )
 }
 
 {
   let m = eu.matrix_t.from_array ([
-    [ 2, -1],
-    [-1,  2],
+    [ 2, -1,  0],
+    [-1,  2, -1],
+    [ 0, -3,  4],
+  ])
+
+  let b = eu.vector_t.from_array ([
+    0,
+    -1,
+    4,
   ])
 
   let v = eu.vector_t.from_array ([
+    0,
+    0,
     1,
-    2,
   ])
 
   m.table ()
   v.table ()
   m.act (v) .table ()
-}
 
+  assert (
+    m.act (v) .eq (b)
+  )
+}
