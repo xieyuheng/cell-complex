01c4eeda8d6fad162efc6dc3eeeef13ba6db4508
Xie Yuheng <xyheme@gmail.com>
2019-04-28 21:52:22 +0800

    up

--- examples/hl.ts
+++ examples/hl.ts
@@ -1,47 +1,3 @@
-# Cicada Language
-
-## Aims
-
-- Implement and test mathematical theories in existing languages.
-- Design new languages to formalize abstract mathematical structures.
-- Questions are welcome -- [issues](https://github.com/xieyuheng/cicada/issues)
-
-## Community
-
-- We enforce C4 as collaboration protocol -- [The C4 RFC](https://rfc.zeromq.org/spec:42/C4)
-- [Style Guide](STYLE-GUIDE.md) -- observe the style of existing code and respect it
-- [Code of Conduct](CODE-OF-CONDUCT.md)
-- Source code -- [github](https://github.com/xieyuheng/cicada), [gitlab](https://gitlab.com/xieyuheng/cicada/)
-- IRC -- [#cicada-language](https://kiwiirc.com/nextclient/irc.freenode.net/#cicada-language)
-- CI -- [gitlab-ci](https://gitlab.com/xieyuheng/cicada/pipelines)
-- [cicada-rs](http://github.com/xieyuheng/cicada-rs) -- an old cicada attempt in rust
-
-## Docs
-
-- [API](https://api.cicada-lang.now.sh)
-
-### Modules
-
-- `eu` euclid
-  - euclidean space -- affine space with dot product
-    - `matrix_t`, `vector_t` and `point_t`
-  - module over euclidean domain -- for generic algorithms
-
-- `cg` combinatorial-game
-  - a game engine for n-player perfect information games
-  - example games:
-    - tic-tac-toe
-    - hackenbush -- [demo](http://hackenbush.combinatorial-game.surge.sh/)
-
-- `cx` cell-complex
-  - cell-complex based low dimensional algebraic topology library
-  - example cell complexes:
-    - torus
-
-- `hl` homology
-  - cellular homology of cell-complex
-
-``` typescript
 import * as cx from "cicada-lang/lib/cell-complex"
 import * as hl from "cicada-lang/lib/homology"
 import * as ut from "cicada-lang/lib/util"
@@ -116,13 +72,3 @@ let report: any = {
 }
 
 ut.log (report)
-```
-
-- [todo] cicada
-  - a dependently-typed programming language
-  - game semantics
-  - logic programming interface
-
-## License
-
-- [GPLv3](LICENSE)
