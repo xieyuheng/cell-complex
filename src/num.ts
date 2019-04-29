c1543a26af6c3ac828ee36dc09c0a4aa2cdb313d
Xie Yuheng <xyheme@gmail.com>
2019-04-30 00:49:39 +0800

    up

--- src/num.ts
+++ src/num.ts
@@ -5,10 +5,12 @@ import * as eu from "./euclid-tobe"
 import { set_t, eqv, not_eqv } from "./abstract/set"
 import { euclidean_domain_t } from "./abstract/ring"
 
+export
 let nums = new set_t <number> ({
   eq: (x, y) => x === y
 })
 
+export
 let domain = eu.domain <number> ({
   elements: nums,
   zero: 0,
