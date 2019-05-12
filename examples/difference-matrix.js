95fc5dfdb2fe338f8b4c592a2f08e8768f56202f
Xie Yuheng <xyheme@gmail.com>
2019-05-12 09:30:01 +0800

    up

---
+++ examples/difference-matrix.js
@@ -0,0 +1,34 @@
+let assert = require ("assert")
+
+let ut = require ("cicada-lang/lib/util")
+let num = require ("cicada-lang/lib/num")
+
+{
+  let A = num.matrix ([
+    [-1, 1, 0, 0],
+    [0, -1, 1, 0],
+    [0, 0, -1, 1],
+  ])
+
+  A.mul (A.transpose ()) .print ()
+}
+
+{
+  let A = num.matrix ([
+    [0, 1, 0, 0],
+    [0, -1, 1, 0],
+    [0, 0, -1, 1],
+  ])
+
+  A.mul (A.transpose ()) .print ()
+}
+
+{
+  let A = num.matrix ([
+    [0, 1, 0, 0],
+    [0, -1, 1, 0],
+    [0, 0, -1, 0],
+  ])
+
+  A.mul (A.transpose ()) .print ()
+}
