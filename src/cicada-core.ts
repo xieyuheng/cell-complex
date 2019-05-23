7de9cdb63b5771b5f066902200133c0c919aca83
Xie Yuheng <xyheme@gmail.com>
2019-05-23 22:59:47 +0800

    up

--- src/cicada-core.ts
+++ src/cicada-core.ts
@@ -1,29 +1,22 @@
-export
-class id_t {}
+export class id_t {}
 
-export
-class name_t {
+export class name_t {
   constructor (
     public str: string,
   ) {}
 }
 
-export
-class body_t {
+export class body_t {
   constructor (
     public map: Map <name_t, obj_t> = new Map ()
   ) {}
 }
 
-export
-abstract class exp_t {}
+export abstract class exp_t {}
 
-export
-abstract class obj_t {}
+export abstract class obj_t {}
 
-export
-class var_obj_t
-extends obj_t {
+export class var_obj_t extends obj_t {
   constructor (
     public id: id_t,
     public name: name_t,
@@ -33,9 +26,7 @@ extends obj_t {
   }
 }
 
-export
-class disj_obj_t
-extends obj_t {
+export class disj_obj_t extends obj_t {
   constructor (
     public name: name_t,
     public sub_type_names: Array <name_t>,
@@ -45,9 +36,7 @@ extends obj_t {
   }
 }
 
-export
-class conj_obj_t
-extends obj_t {
+export class conj_obj_t extends obj_t {
   constructor (
     public name: name_t,
     public body: body_t,
@@ -56,9 +45,7 @@ extends obj_t {
   }
 }
 
-export
-class data_obj_t
-extends obj_t {
+export class data_obj_t extends obj_t {
   constructor  (
     public name: name_t,
     public body: body_t,
@@ -67,13 +54,9 @@ extends obj_t {
   }
 }
 
-export
-class tt_obj_t
-extends obj_t {}
+export class tt_obj_t extends obj_t {}
 
-export
-class module_obj_t
-extends obj_t {
+export class module_obj_t extends obj_t {
   constructor (
     public body: body_t = new body_t (),
   ) {
