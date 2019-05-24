9fd6557f503686eeca7c87cf5ad09cd88adc97c5
Xie Yuheng <xyheme@gmail.com>
2019-05-24 22:37:39 +0800

    up

--- examples/cicada/simple-game-semantics.js
+++ examples/cicada/simple-game-semantics.js
@@ -1,45 +1,42 @@
 let cc = require ("../../lib/cicada/simple")
 
-{
+let true_t = cc.conj ("true_t", {})
+let false_t = cc.conj ("false_t", {})
+let bool_t = cc.disj ("bool_t", [ true_t, false_t ])
 
-  let true_t = cc.conj ("true_t", {})
-  let false_t = cc.conj ("false_t", {})
-  let bool_t = cc.disj ("bool_t", [ true_t, false_t ])
+bool_t
+  .report ()
 
-  bool_t
-    .report ()
+bool_t
+  .dot ("true_t")
+  .report ()
 
-  bool_t
-    .dot ("true_t")
-    .report ()
+let f1_t = cc.arrow ({
+  "x": bool_t, "y": bool_t
+}, bool_t)
 
-  let f1_t = cc.arrow ({
-    "x": bool_t, "y": bool_t
-  }, bool_t)
+console.log (0)
 
-  console.log (0)
+f1_t
+  .report ()
 
-  f1_t
-    .report ()
+console.log (1)
 
-  console.log (1)
+f1_t
+  .dot ("true_t")
+  .report ()
 
-  f1_t
-    .dot ("true_t")
-    .report ()
+console.log (2)
 
-  console.log (2)
+f1_t
+  .dot ("true_t")
+  .dot ("false_t")
+  .report ()
 
-  f1_t
-    .dot ("true_t")
-    .dot ("false_t")
-    .report ()
+console.log (3)
 
-  console.log (3)
-
-  f1_t
-    .dot ("true_t")
-    .dot ("false_t")
-    .dot ("false_t")
-    .report ()
-}
+f1_t
+  .dot ("true_t")
+  .dot ("false_t")
+  .dot ("false_t")
+  .report ()
