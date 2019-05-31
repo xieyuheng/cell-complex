cd819222bc865c4e48047758f9c128dbd7a24c68
Xie Yuheng <xyheme@gmail.com>
2019-05-31 20:39:30 +0800

    up

--- src/cicada/std/list.ts
+++ src/cicada/std/list.ts
@@ -2,8 +2,8 @@ import * as cc from "../core"
 
 export
 let list = new cc.module_t ("list")
-  .union ("list_t", [ "null_t", "cons_t" ], { "t": "type" })
-  .record ("null_t", { "t": "type" })
+// .union ("list_t", [ "null_t", "cons_t" ], { "t": "type" })
+// .record ("null_t", { "t": "type" })
 // TODO
 //   .record ("cons_t", {
 //     "t": "type",
