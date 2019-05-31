1df3b95897d55aa506226cf77908e94f2d2811e0
Xie Yuheng <xyheme@gmail.com>
2019-05-31 21:12:56 +0800

    - vect.ts -- with TODO

---
+++ src/cicada/std/vect.ts
@@ -0,0 +1,29 @@
+import * as cc from "../core"
+import { nat } from "./nat"
+
+export
+let vect = new cc.module_t ("vect")
+  // .use (nat)
+  // .union ("vect_t", [
+  //   "vect_null_t",
+  //   "vect_cons_t",
+  // ], {
+  //   "t": "type",
+  //   "length": "nat_t",
+  // })
+  // .record ("vect_null_t", {
+  //   "t": "type",
+  //   "length": "zero_t",
+  // })
+  // .record ("vect_cons_t", {
+  //   "[implicit]" {
+  //     "n": "nat_t",
+  //   },
+  //   "t": "type",
+  //   "length": apply ("succ_t", dot ("this", "n")),
+  //   "car": dot ("this", "t"),
+  //   "cdr": apply ("vect_t", {
+  //     "t": dot ("this", "t"),
+  //     "length": dot ("this", "n"),
+  //   }),
+  // })
