219ffc2cefdc7a969c31e7587f5d80e2198c4f8a
Xie Yuheng <xyheme@gmail.com>
2019-06-03 12:19:24 +0800

    up

--- src/cicada/std/list.ts
+++ src/cicada/std/list.ts
@@ -8,26 +8,26 @@ import { ref_t } from "../ref"
 export
 function list (): cc.module_t {
   let m = new cc.module_t ("list")
-  m.define ("list_t", new union_t ("list_t", [
-    new ref_t (m, "null_t"),
-    new ref_t (m, "cons_t"),
-  ]))
-  m.define ("null_t", new record_t ("null_t", {
-    t: new type_t (),
-  }))
-  m.define ("cons_t", new record_t ("cons_t", {
-    t: new type_t (),
-    car: new this_t ("t"),
-    cdr: m.game ("list_t") .choices ({
-      t: new this_t ("t"),
-    }),
-  }))
-  m.define ("cons_t", new record_t ("cons_t", {
-    t: new type_t (),
-    car: new this_t ("t"),
-    cdr: m.game ("list_t") .choices ({
-      t: new this_t ("t"),
-    }),
-  }))
+  // m.define ("list_t", new union_t ("list_t", [
+  //   new ref_t (m, "null_t"),
+  //   new ref_t (m, "cons_t"),
+  // ]))
+  // m.define ("null_t", new record_t ("null_t", {
+  //   t: new type_t (),
+  // }))
+  // m.define ("cons_t", new record_t ("cons_t", {
+  //   t: new type_t (),
+  //   car: new this_t ("t"),
+  //   cdr: m.game ("list_t") .choices ({
+  //     t: new this_t ("t"),
+  //   }),
+  // }))
+  // m.define ("cons_t", new record_t ("cons_t", {
+  //   t: new type_t (),
+  //   car: new this_t ("t"),
+  //   cdr: m.game ("list_t") .choices ({
+  //     t: new this_t ("t"),
+  //   }),
+  // }))
   return m
 }
