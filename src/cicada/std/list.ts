7f044dee2b5a912825ec01dc815b23bdfda69a4d
Xie Yuheng <xyheme@gmail.com>
2019-06-05 22:29:50 +0800

    up

--- src/cicada/std/list.ts
+++ src/cicada/std/list.ts
@@ -7,15 +7,15 @@ import { ref_t } from "../ref"
 
 let m = new cc.module_t ("list")
 
-// m.define ("list_t", new union_builder_t ("list_t", [
-//   new ref_t ("null_t"),
-//   new ref_t ("cons_t"),
-// ], _map => ({
-// })))
+m.define ("list_t", new union_builder_t ("list_t", [
+  new ref_t ("null_t"),
+  new ref_t ("cons_t"),
+], _map => ({
+})))
 
-// m.define ("null_t", new record_builder_t ("null_t", _map => ({
-//   "t": new type_t (),
-// })))
+m.define ("null_t", new record_builder_t ("null_t", _map => ({
+  "t": new type_t (),
+})))
 
 // m.define ("cons_t", new record_builder_t ("cons_t", map => ({
 //   "t": new type_t (),
--- src/cicada/std/vect.ts
+++ src/cicada/std/vect.ts
@@ -8,20 +8,20 @@ import { nat } from "./nat"
 
 let m = new cc.module_t ("vect")
 
-// m.use (nat ())
+// m.use (nat)
 
-// m.define ("vect_t", new union_builder_t ("vect_t", [
-//   new ref_t ("vect_null_t"),
-//   new ref_t ("vect_cons_t"),
-// ], map => ({
-//   t: new type_t (),
-//   length: new ref_t ("nat_t"),
-// })))
+m.define ("vect_t", new union_builder_t ("vect_t", [
+  new ref_t ("vect_null_t"),
+  new ref_t ("vect_cons_t"),
+], map => ({
+  t: new type_t (),
+  length: new ref_t ("nat_t"),
+})))
 
-// m.define ("vect_null_t", new record_builder_t ("vect_null_t", map => ({
-//   t: new type_t (),
-//   length: new ref_t ("zero_t"),
-// })))
+m.define ("vect_null_t", new record_builder_t ("vect_null_t", map => ({
+  t: new type_t (),
+  length: new ref_t ("zero_t"),
+})))
 
 // m.define ("vect_cons_t", new record_builder_t ("vect_cons_t", map => ({
 //   [implicit]: {
